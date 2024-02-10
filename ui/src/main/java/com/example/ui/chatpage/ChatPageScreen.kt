d09f4c0ae794e541e19fe11dff0e06d02af1fb67
Khaled Alramam <khaledalramam@gmail.com>
2024-02-10 17:09:55 +0200

    Add ChatPageScreen: - include need design - wrap it up with viewModel and logic needed

---
+++ ui/src/main/java/com/example/ui/chatpage/ChatPageScreen.kt
@@ -0,0 +1,57 @@
+package com.example.ui.chatpage
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.ui.R
+import com.example.ui.components.ChatScreenHeader
+import com.example.ui.components.ChatTextFieldWithSendButton
+import com.example.ui.components.MessageItem
+import com.example.ui.viewmodel.ChatPageViewModel
+import kotlinx.coroutines.launch
+
+@Composable
+fun ChatPageScreen() {
+    val chatViewModel: ChatPageViewModel = viewModel()
+    val coroutineScope = rememberCoroutineScope()
+    val messages by chatViewModel.messages.collectAsState()
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(top = 70.dp, start = 16.dp, end = 16.dp, bottom = 16.dp)
+    ) {
+        ChatScreenHeader("Mohammed", {}, {})
+        LazyColumn(
+            modifier = Modifier
+                .weight(1f)
+                .padding(8.dp),
+            verticalArrangement = Arrangement.spacedBy(10.dp)
+        ) {
+            items(items = messages) {
+                MessageItem(
+                    text = it.content, time = "11:00", myMessage = true, image = painterResource(
+                        id = R.drawable.my_avatar
+                    )
+                )
+            }
+        }
+
+        ChatTextFieldWithSendButton(onSendClicked = {
+            coroutineScope.launch {
+                chatViewModel.sendMessage(it)
+            }
+        })
+    }
+}
